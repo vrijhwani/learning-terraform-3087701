f8eebf4b3304fb0d7fce87812e88e8a4d51ce34b
vrijhwani <vross29@gmail.com>
2023-05-06 22:30:35 -0700

    Vpc-subnet

--- main.tf
+++ main.tf
@@ -34,7 +34,7 @@ module "blog_vpc" {
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  subnet_id              = module.blog_vpc.public_subnets[0]
+  
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
@@ -46,8 +46,8 @@ module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.13.0"
 
-  vpc_id  = module.blog_vpc.vpc_id
   name    = "blog"
+  vpc_id  = module.vpc.public_subnets[0]
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
