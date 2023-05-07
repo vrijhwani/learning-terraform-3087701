3ee685db3b851be863b3fb20b3d0ad20ed48d673
vrijhwani <vross29@gmail.com>
2023-05-06 17:26:49 -0700

    Update main.tf
    Add security groups

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   from_port   = 80
   to_port     = 80
   protocol    = "tcp"
-  cidr_blocks = [0.0.0.0/0]
+  cidr_blocks = ["0.0.0.0/0"]
   
   security_group_id = aws_security_group.blog.id
 }
@@ -52,7 +52,7 @@ resource "aws_security_group_rule" "blog_https_in"{
   from_port   = 443
   to_port     = 443
   protocol    = "tcp"
-  cidr_blocks = [0.0.0.0/0]
+  cidr_blocks = ["0.0.0.0/0"]
   
   security_group_id = aws_security_group.blog.id
 }
@@ -62,7 +62,7 @@ resource "aws_security_group_rule" "blog_everything_out"{
   from_port = 0
   to_port = 0
   protocol = "-1"
-  cidr_blocks = [0.0.0.0/0]
+  cidr_blocks = ["0.0.0.0/0"]
   
   security_group_id = aws_security_group.blog.id
 }
