a0c4d7620c4edb7d125ca9e82fff513a071a957a
vrijhwani <vross29@gmail.com>
2023-05-06 22:35:07 -0700

    private subnets added

--- main.tf
+++ main.tf
@@ -21,6 +21,7 @@ module "blog_vpc" {
   cidr = "10.0.0.0/16"
 
   azs             = ["us-west-2a","us-west-2b","us-west-2c"]
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
   public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   enable_nat_gateway = true
@@ -34,7 +35,7 @@ module "blog_vpc" {
 resource "aws_instance" "blog" {
   ami                    = data.aws_ami.app_ami.id
   instance_type          = var.instance_type
-  
+
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   tags = {
